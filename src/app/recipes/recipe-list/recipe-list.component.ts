8b7b785e09e95a752304765dc49461210bbca3b1
Austin Barthel <73510383+abarthel11@users.noreply.github.com>
2024-01-04 16:38:28 -0700

    updates

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('A Test Recipe', 'The is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+}
