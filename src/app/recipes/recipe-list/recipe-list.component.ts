2f77bf0e40732037b33d611aa2ae42d6319c5708
Austin Barthel <73510383+abarthel11@users.noreply.github.com>
2024-01-09 17:03:56 -0700

    recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,33 +1,24 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css'],
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+	recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Another Test Recipe',
-      'The is simply a test',
-      'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'The is simply a test',
-      'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'
-    ),
-  ];
+	onRecipeSelected(recipe: Recipe) {
+		this.recipeWasSelected.emit(recipe);
+	}
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+	constructor(private recipeService: RecipeService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+	ngOnInit(): void {
+		this.recipes = this.recipeService.getRecipes();
+	}
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,21 @@
+import { Recipe } from './recipe.model';
+export class RecipeService {
+  constructor() { }
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Another Test Recipe',
+      'The is simply a test',
+      'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'The is simply a test',
+      'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
